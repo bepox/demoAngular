fe7577b02bcf008ffcd9dd31c39f49751505178a
eray <eray@erays-MacBook-Pro-2.local>
2020-05-13 17:55:36 +0200

    add user component and add style.css

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  user : User = {
+    id: 1,
+    name: 'Eray'
+  };
 
   ngOnInit(): void {
   }
-  user = 'Eray';
+  
 }
